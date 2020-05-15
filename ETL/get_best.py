7d0649ce89497d2cfed60641b5bebdb08450782e
Robert Hillery <robert.hillery@gmail.com>
2020-05-15 19:26:05 -0400

    best seller script framework

---
+++ ETL/get_best.py
@@ -0,0 +1,18 @@
+import re
+import csv
+import time, random
+import requests
+import pandas as pd
+import urllib.request
+from tqdm import tqdm
+from bs4 import BeautifulSoup as bs
+
+
+def get_urls(base_url):
+    pass 
+
+def get_books(weekly_url):
+    """
+    will get weeks on the list, title, and author
+    """
+    pass
