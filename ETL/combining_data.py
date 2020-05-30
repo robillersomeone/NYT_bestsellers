2a1b7524801de91e8df77c2f16e00c97475c401a
Robert Hillery <robert.hillery@gmail.com>
2020-05-30 14:59:24 -0400

    update ETL workflow for best sellers

--- ETL/combining_data.py
+++ ETL/combining_data.py
@@ -9,13 +9,27 @@ gr_2019_df = pd.read_csv('../data/2019_goodreads.csv')
 # need to account for list elements in 'publisher' and 'data' in 2016-18 data
 # if there are two dates, index 375 in 2016, take first element
 # if there are null values
+choiceawards_df = pd.concat([gr_2016_df, gr_2017_df, gr_2018_df, gr_2019_df])
 
-# drop duplicate titles
-# column for bs
-# concat not bs dataframes
-# add column for non bs
-# concat bs and not
+print('total books', choiceawards_df.shape)
+
 
 # check duplicates
+doubled_books_df = choiceawards_df[choiceawards_df.duplicated(['title'], keep=False)]
+
+print('doubled books', doubled_books_df.shape)
+
+# drop duplicate titles
+df = choiceawards_df.drop_duplicates(subset='title', keep='last')
+
+# add column for best seller
+# set to 0 to start
+
+
+# read from database
+# if the title is in the database, set to 1
+
+
 
 # export to csv
+# book_df.to_csv('../data/16_19_goodreads.csv')
--- ETL/get_best.py
+++ ETL/get_best.py
@@ -4,9 +4,13 @@ import time, random
 import requests
 import pandas as pd
 import urllib.request
+from config import url_best_seller
 from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
 
+# get weekly best sellers
+
+# most likely insert into a local db (postgres)
 
 def get_urls(base_url):
     pass 
@@ -14,5 +18,21 @@ def get_urls(base_url):
 def get_books(weekly_url):
     """
     will get weeks on the list, title, and author
+
     """
+    bs_list = []
+    get_title = pass
+    get_author = pass
+    weeks = pass
+     bs_list.append({'title':get_title, 
+                    'author':get_author,
+                    'weeks': weeks})
+    pass
+
+
+def insert_into_db():
+    """
+    COPY output from get_books() into db
+    """
+
     pass
